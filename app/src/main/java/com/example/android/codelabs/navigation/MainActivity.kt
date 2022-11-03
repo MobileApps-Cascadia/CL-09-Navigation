088f4b494072b605fddb01eb1006e77e64837147
Dr. Brian Bansenauer <brianb@cascadia.edu>
2022-11-03 13:04:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/codelabs/navigation/MainActivity.kt
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.navigation
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.navigation.NavigationView
+
+/**
+ * A simple activity demonstrating use of a NavHostFragment with a navigation drawer.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.navigation_activity)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        // Set up Action Bar
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        // TODO STEP 9.5 - Create an AppBarConfiguration with the correct top-level destinations
+        // You should also remove the old appBarConfiguration setup above
+
+        // TODO END STEP 9.5
+
+        setupActionBar(navController, appBarConfiguration)
+
+        setupNavigationMenu(navController)
+
+        setupBottomNavMenu(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+
+            Toast.makeText(this@MainActivity, "Navigated to $dest",
+                    Toast.LENGTH_SHORT).show()
+            Log.d("NavigationActivity", "Navigated to $dest")
+        }
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        // TODO STEP 9.3 - Use NavigationUI to set up Bottom Nav
+
+        // TODO END STEP 9.3
+    }
+
+    private fun setupNavigationMenu(navController: NavController) {
+        // TODO STEP 9.4 - Use NavigationUI to set up a Navigation View
+//        // If you rotate the screen, you can drag this view out from the left
+//        // This does NOT modify the actionbar
+
+        // TODO END STEP 9.4
+    }
+
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig : AppBarConfiguration) {
+        // TODO STEP 9.6 - Have NavigationUI handle what your ActionBar displays
+//        // This allows NavigationUI to decide what label to show in the action bar
+//        // By using appBarConfig, it will also determine whether to
+//        // show the up arrow or drawer menu icon
+
+        // TODO END STEP 9.6
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val retValue = super.onCreateOptionsMenu(menu)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        // The NavigationView already has these same navigation items, so we only add
+        // navigation items to the menu here if there isn't a NavigationView
+        if (navigationView == null) {
+            menuInflater.inflate(R.menu.overflow_menu, menu)
+            return true
+        }
+        return retValue
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        // TODO STEP 9.2 - Have Navigation UI Handle the item selection - replace the return statement above
+        // Have the NavigationUI look for an action or destination matching the menu item id and navigate there if found.
+        // Otherwise, bubble up to the parent.
+
+        // TODO END STEP 9.2
+    }
+
+    // TODO STEP 9.7 - Have NavigationUI handle up behavior in the ActionBar
+//    override fun onSupportNavigateUp(): Boolean {
+
+//    }
+    // TODO END STEP 9.7
+}
