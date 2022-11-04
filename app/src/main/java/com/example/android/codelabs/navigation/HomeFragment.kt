8e1aff6477e3ca989adb2c8b7b689f127a145ba3
Tori Lew <torirlew@gmail.com>
2022-11-03 20:03:47 -0700

    completed all todo's and changed a animation to be unique

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -18,7 +18,11 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -37,19 +41,38 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-
+        /*val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null)
+        }*/
         //TODO END STEP 5
 
         //TODO STEP 6 - Set NavOptions
-
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
         //TODO END STEP 6
 
         //TODO STEP 7 - Update the OnClickListener to navigate using an action
-
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.next_action, null)
+        )
         //TODO END STEP 7
 
         //TODO STEP 8 - Update the OnClickListener to navigate using a direction
-
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener{
+            val flowStepNumberArg = 1
+            val action = HomeFragmentDirections.nextAction(flowStepNumberArg)
+            findNavController().navigate(action)
+        }
         //TODO END STEP 8
     }
 
